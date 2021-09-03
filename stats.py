f908dede921351a3407c21c9190a100b5c994006
Matheus Martins <3mhenrique@gmail.com>
2021-09-02 21:09:06 -0300

    Atualização massiva de configuração
    -Adicionado função de relógio;
    -Atualizados os comentários;
    -Mantidas as referências do script inicial;
    -Adicionado try para caso de interrupção de sistema ou por teclado.

--- stats.py
+++ stats.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright (c) 2017 Adafruit Industries
 # Author: Tony DiCola & James DeVito
 # Modified : Matheus Martins
@@ -18,12 +20,17 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-import time
-import sys 
 
-import Adafruit_GPIO.SPI as SPI 
-import Adafruit_SSD1306
+# Necessário importar template do display em https://github.com/adafruit/Adafruit_SSD1306
+
+# Importando as bibliotecas
+import time
+import sys
 import signal
+import datetime
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
 from PIL import Image
 from PIL import ImageDraw
@@ -31,106 +38,126 @@ from PIL import ImageFont
 
 import subprocess
 
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
+# Configuração dos pinos da Raspberry Pi
+RST = None
+# Apenas no modo SPI para ser usado
 DC = 23
-SPI_PORT = 0 
-SPI_DEVICE = 0 
+SPI_PORT = 0
+SPI_DEVICE = 0
 
-# 128x32 display with hardware I2C:
 disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-# Initialize library.
+# Inicializando a biblioteca
 disp.begin()
 
-# Clear display.
+# Limpando a tela
 disp.clear()
 disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
 width = disp.width
 height = disp.height
 image = Image.new('1', (width, height))
 
-# Get drawing object to draw on image.
+# "Limpando" a tela preenchendo com fundo preto
 draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
+# Preparando a tela
 padding = -2
 top = padding
 bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
+# Move o sinalizador para o canto esquerdo para iniciar escrita
 x = 0
 
-# Load default font.
+# Carregando as fontes para projeto
+## Outras fontes podem ser obtidas em http://www.datafont.com/bitmap.php
 font = ImageFont.load_default()
+font2 = ImageFont.truetype('digital-7.mono.ttf', 24)
 
-#Função para interrupção de script e limpar a tela pós shutdown
-def trap_python(signum, stack):
+# Informacoes de hora e data
+dateString = '%d %B %Y'
+timeString = '%H:%M'
+
+# Função de interrupção para limpeza da tela (trap)
+def kill_signal(signum, frame):
     print("Recebido Control-C")
     disp.clear()
     disp.display()
     sys.exit(0)
 
-while True:
-    #Chamada na função em caso de interrupção
-    signal.signal(signal.SIGINT, trap_python)
-    signal.signal(signal.SIGHUP, trap_python)
-    signal.signal(signal.SIGTERM, trap_python)
+# Código em loop com as telas de hora,sistema e status
+# Em breve novas func.
+try:
+    while True:
+        # Sinais de interrupção chamadno a função kill_signal
+        signal.signal(signal.SIGINT, kill_signal)
+        signal.signal(signal.SIGHUP, kill_signal)
+        signal.signal(signal.SIGTERM, kill_signal)
 
-    #Desenhando um retangulo para receber conteudo
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+        # Atribuição de hora e data
+        strDate = datetime.datetime.now().strftime(dateString)
+        result  = datetime.datetime.now().strftime(timeString)
 
-    #cmd = "hostname -I | cut -d\' \' -f1"
-    cmd = "curl -s ifconfig.me"
-    IP = subprocess.check_output(cmd, shell = True )
-    #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    cmd = "uptime  | grep -o 'load.*' | sed s/\ average// | sed s/,\ /\|/g"
-    CPU = subprocess.check_output(cmd, shell = True )
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
+        # "Limpando" a tela preenchendo com fundo preto
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    #Escrevendo as 4 linhas da primeira sessao
+        # Texto de data em duas linhas.
+        draw.text((x+15,top),strDate, font=font,fill=255)
+        draw.text((x+35, top+14), result,  font=font2, fill=255)
+        draw.line((0, top+12, 127, top+12), fill=100)
 
-    draw.text((x, top),       IP,  font=font, fill=255)
-    draw.text((x, top+8),     CPU, font=font, fill=255)
-    draw.text((x, top+16),    MemUsage,  font=font, fill=255)
-    draw.text((x, top+25),    Disk,  font=font, fill=255)
+        # Exibindo a imagem com timer.
+        disp.image(image)
+        disp.display()
+        time.sleep(35)
 
-    #Exibindo as primeira sessao
-    disp.image(image)
-    disp.display()
-    time.sleep(10)
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    #Limpa tela para segunda sessao
-    draw.rectangle((0,0,width,height), outline=0, fill=0)
+        #cmd = "hostname -I | cut -d\' \' -f1"
+        cmd = "curl -s ifconfig.me"
+        IP = subprocess.check_output(cmd, shell = True )
+        #cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        cmd = "uptime  | grep -o 'load.*' | sed s/\ average// | sed s/,\ /\|/g"
+        CPU = subprocess.check_output(cmd, shell = True )
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell = True )
+        cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+        Disk = subprocess.check_output(cmd, shell = True )
 
-    #Escrevendo as 4 linhas da segunda sessao
-    cmd = "echo Hostname: $(hostname)"
-    Hname = subprocess.check_output(cmd, shell = True)
-    cmd = "sensors | grep temp | awk '{print \" Temp: \" $2}' | cut -c2-12"
-    Temp = subprocess.check_output(cmd, shell = True)
-    cmd = "uptime | awk -F' ' '{print \"Uptime: \" $3$4}' | sed s/,//"
-    Utime = subprocess.check_output(cmd, shell = True)
-    cmd = "echo Data: $(date +%d/%m/%y_%H:%M)"
-    Date= subprocess.check_output(cmd, shell = True)
+        # Escrevendo as 4 linhas da primeira sessao
+        draw.text((x, top),       IP,  font=font, fill=255)
+        draw.text((x, top+8),     CPU, font=font, fill=255)
+        draw.text((x, top+16),    MemUsage,  font=font, fill=255)
+        draw.text((x, top+25),    Disk,  font=font, fill=255)
 
-    #Exibindo a segunda sessao
-    draw.text((x, top),       Hname,  font=font, fill=255)
-    draw.text((x, top+8),     Temp, font=font, fill=255)
-    draw.text((x, top+16),    Utime,  font=font, fill=255)
-    draw.text((x, top+25),    Date,  font=font, fill=255)
+        # Exibindo as primeira sessao
+        disp.image(image)
+        disp.display()
+        time.sleep(10)
 
-    disp.image(image)
-    disp.display()
-    time.sleep(10)
+        # Limpa tela para segunda sessao
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-  
+        # Escrevendo as 4 linhas da segunda sessao
+        cmd = "echo Hostname: $(hostname)"
+        Hname = subprocess.check_output(cmd, shell = True)
+        cmd = "sensors | grep temp | awk '{print \" Temp: \" $2}' | cut -c2-12"
+        Temp = subprocess.check_output(cmd, shell = True)
+        cmd = "uptime | awk -F' ' '{print \"Uptime: \" $3$4}' | sed s/,//"
+        Utime = subprocess.check_output(cmd, shell = True)
+        cmd = "echo Data: $(date +%d/%m/%y_%H:%M)"
+        Date= subprocess.check_output(cmd, shell = True)
+
+        # Exibindo as info da proxima tela
+        draw.text((x, top),       Hname,  font=font, fill=255)
+        draw.text((x, top+8),     Temp, font=font, fill=255)
+        draw.text((x, top+16),    Utime,  font=font, fill=255)
+        draw.text((x, top+25),    Date,  font=font, fill=255)
+
+        disp.image(image)
+        disp.display()
+        time.sleep(10)
+
+except (KeyboardInterrupt, BrokenPipeError, SystemExit): # Se houver interrupcao ou erro no while sai do programa limpando o display
+    print("Display limpo!")
+    GPIO.cleanup()
