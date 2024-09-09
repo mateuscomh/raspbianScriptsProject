71eefe6b60c90496ec2ebb73f50b6ad29110115d
Matheus Martins <matheuscumth@gmail.com>
2024-09-09 15:46:50 -0300

    add telegram notification

--- mountcheck.sh
+++ mountcheck.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+source /scripts/ENV
 ponto_montagem="/mnt/Orico01S"
 #docker stop $(docker ps -a -q) && sleep 5
 if grep -qs "$ponto_montagem" /proc/mounts; then
     exit 0
 else
-    mount -a
+    echo "$(date) Tentativa de montagem $ponto_montagem" >> /tmp/montagem
+    mount -a || curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+    -d chat_id="${CHAT_ID}" -d text="[⚠️ Atenção] $ponto_montagem não está acessível em $(hostname)" 2>&1 1>/dev/null ; exit 1
     exit 0
 fi
