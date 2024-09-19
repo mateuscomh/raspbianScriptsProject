013447a0ba07412410c87e23f9330c951e3d05f8
Matheus Martins <3mhenrique@gmail.com>
2024-09-19 15:17:09 -0300

    Update mountcheck.sh

--- mountcheck.sh
+++ mountcheck.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 source /scripts/ENV
 ponto_montagem="/mnt/Orico01S"
-#docker stop $(docker ps -a -q) && sleep 5
-if grep -qs "$ponto_montagem" /proc/mounts; then
+if [ -d "$ponto_montagem" ] && ls "$ponto_montagem" > /dev/null 2>&1; then
     exit 0
 else
     echo "$(date) Tentativa de montagem $ponto_montagem" >> /tmp/montagem
-    mount -a || curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
-    -d chat_id="${CHAT_ID}" -d text="[⚠️ Atenção] $ponto_montagem não está acessível em $(hostname)" 2>&1 1>/dev/null ; exit 1
+    mount -a
+    ls "$ponto_montagem" || curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+        -d chat_id="${CHAT_ID}" \
+        -d text="[⚠️ Atenção] $ponto_montagem não está acessível em $(hostname)" 2>&1 1>/dev/null
     exit 0
 fi
