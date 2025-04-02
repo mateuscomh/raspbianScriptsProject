93dcca3354d6685970d519bc6e707c0e82db5d5d
Matheus Martins <3mhenrique@gmail.com>
2025-04-01 21:20:09 -0300

    Update mountcheck.sh

--- mountcheck.sh
+++ mountcheck.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 source /scripts/ENV
-ponto_montagem="/mnt/Orico01S"
-if [ -d "$ponto_montagem" ] && ls "$ponto_montagem" > /dev/null 2>&1; then
+ponto_montagem="/mnt/ResticBackup"
+if [ -d "$ponto_montagem" ] && ls "$ponto_montagem/checkpointMorpheus.tmp" > /dev/null 2>&1; then
     exit 0
 else
     echo "$(date) Tentativa de montagem $ponto_montagem" >> /tmp/montagem
     mount -a
-    ls "$ponto_montagem" || curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+    ls "$ponto_montagem/Django" && echo "$(date) montagem ok" || curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
         -d chat_id="${CHAT_ID}" \
         -d text="[⚠️ Atenção] $ponto_montagem não está acessível em $(hostname)" 2>&1 1>/dev/null
     exit 0
